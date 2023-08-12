ccbc4561a6794d0888a2ec626f17f00e531e9ebc
Xiaojian "JJ" Deng <xjd001@gmail.com>
2023-08-12 09:43:32 -0400

    Update cleanup.py

--- cleanup.py
+++ cleanup.py
@@ -1,5 +1,6 @@
 from fastai.vision.all import *
 from path import Path as path
+from tqdm import tqdm
 
 def run(mydir, bs = 16, thresh = 0.1, cpu = False, modfile = None, place = 0):
     if not modfile:
@@ -12,6 +13,7 @@ def run(mydir, bs = 16, thresh = 0.1, cpu = False, modfile = None, place = 0):
     files = path(mydir).files()
     if len(files) == 0:
         return
+    """
     test_dl = learn.dls.test_dl(files, with_label=True)
     test_dl.bs = bs
     pred_probas, _, _ = learn.get_preds(dl=test_dl, with_decoded=True)
@@ -21,3 +23,16 @@ def run(mydir, bs = 16, thresh = 0.1, cpu = False, modfile = None, place = 0):
             f.move(baddir)
         else:
             f.move(gooddir)
+    """
+    with learn.no_bar(), learn.no_logging():
+        for f in tqdm(files):
+            #print(learn.predict(f))
+            try:
+                pred = learn.predict(f)[2][0]
+            except Exception as e:
+                f.move(baddir)
+                continue
+            if pred > thresh:
+                f.move(baddir)
+            else:
+                f.move(gooddir)
